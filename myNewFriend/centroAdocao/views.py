e0c3c490eaa418c9fc9495599237b748ca4e5d29
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-08-18 21:50:02 -0300

    liga as duas paginas

--- myNewFriend/centroAdocao/views.py
+++ myNewFriend/centroAdocao/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def post_list(request):
+ return HttpResponse('Olá Mundo!')
--- myNewFriend/home/views.py
+++ myNewFriend/home/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def post_list(request):
- return HttpResponse('Olá Mundo!')
+ return render(request, 'index.html')
