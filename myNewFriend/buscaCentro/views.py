4e7c4f312651ac822d67d79b220c6b12235a1580
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-09-22 18:46:20 -0300

    estrutura basica pronta

--- myNewFriend/buscaCentro/views.py
+++ myNewFriend/buscaCentro/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from centroAdocao.models import DadosCentroAdocao
 
-# Create your views here.
+import logging
+
+logger = logging.getLogger(__name__)
+
+def resultadoBusca(request):
+    lista = DadosCentroAdocao.objects.all()
+
+    busca = request.GET.get("busca")
+    if(busca):
+        lista = DadosCentroAdocao.objects.filter(nome__contains = busca)
+
+    logger.error(lista)
+    return render(request, 'busca.html', {'Resultado': lista})
